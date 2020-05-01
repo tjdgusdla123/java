37d64cf69dbc98c8e87b2b2f293a86835efa972d
tjdgusdla123 <dkxkgh98@naver.com>
2020-05-01 10:47:56 +0900

    0406

---
+++ Java Application0406/src/javaapp0406/Display.java
@@ -0,0 +1,10 @@
+package javaapp0406;
+
+public class Display {
+
+	public static void main(String[] args) {
+		System.out.println("임성현");
+
+	}
+
+}
---
+++ Java Application0406/src/javaapp0406/First.java
@@ -0,0 +1,13 @@
+package javaapp0406;
+
+public class First {
+
+	public static void main(String[] args) {
+		//모니터에 텍스트를 출력해주는 코드
+		/*
+		 여러 줄 주석
+		 */
+		System.out.println("Hello Java");
+		
+	}
+}
