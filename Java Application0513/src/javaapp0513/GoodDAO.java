68cd8fe50433ddddfc02708d8a611efae27a9e5b
tjdgusdla123 <dkxkgh98@naver.com>
2020-05-14 17:23:16 +0900

    0513

--- Java Application0513/src/javaapp0513/GoodDAO.java
+++ Java Application0513/src/javaapp0513/GoodDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 //데이터베이스 연동 메소드를 소유한 클래스
 public class GoodDAO {
-//싱글톤 패턴 디자인 - 객체를 1개만 생성할 수 있도록 만든 패턴
+	// 싱글톤 패턴 디자인 - 객체를 1개만 생성할 수 있도록 만든 패턴
 	// 외부에서 생성할 수 없도록 생성자를 private으로
 	private GoodDAO() {
 	}
@@ -17,7 +17,8 @@ public class GoodDAO {
 	// 만들어진 객체의 참조를 저장하기 위한 변수
 	private static GoodDAO goodDAO = null;
 
-	// 객체가 없으면 만들어서 리턴 // 있으면 있는 것을 리턴하도록 하는 객체 사용을 위한 메소드
+	// 객체가 없으면 만들어서 리턴하고
+	// 있으면 있는 것을 리턴하도록 하는 객체 사용을 위한 메소드
 	public static GoodDAO sharedInstance() {
 		if (goodDAO == null) {
 			goodDAO = new GoodDAO();
@@ -34,25 +35,27 @@ public class GoodDAO {
 		try {
 			con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.200:1521:xe", "user10", "user10");
 		} catch (Exception e) {
-			System.out.println("연결 실패");
-			System.out.println(e.getMessage());
-		}
-	}
-
-	//
-	private void close() {
-		try {
-			pstmt.close();
-			con.close();
-		} catch (Exception e) {
 			System.err.println("연결 실패");
 			System.err.println(e.getMessage());
 		}
 	}
 
-	// 전체 데이터를 가져오는 메소드 // select 는 where 절을 확인해서 매개변수 생성 // select* from good;
+	// 데이터베이스 연결 해제 하는 메소드
+	private void close() {
+		try {
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			System.err.println("연결 해제 실패");
+			System.err.println(e.getMessage());
+		}
+	}
+
+	// 전체 데이터를 가져오는 메소드
+	// select는 where 절을 확인해서 매개변수를 생성
+	// select * from goods;
 	// List를 리턴할 때는 인스턴스를 만들고 데이터를 추가한 후 리턴
-	// 여러 개의 데이터를 가져올 때는 데이터가 없는 경우 size가0
+	// 여러 개의 데이터를 가져올 때는 데이터가 없는 경우 size가 0
 	public List<Good> allGood() {
 		// 리턴할 데이터 생성
 		// 여러 개 일 때는 생성자를 호출해서 인스턴스를 생성
@@ -62,16 +65,19 @@ public class GoodDAO {
 		connect();
 		try {
 			// sql 실행 객체 생성
-			pstmt = con.prepareStatement(" select* from goods");
+			pstmt = con.prepareStatement("select * from goods");
+			// sql 실행
 			ResultSet rs = pstmt.executeQuery();
 			// 데이터가 여러 개
 			while (rs.next()) {
 				// 행 단위 작업 수행
 				Good imsi = new Good();
 				// code 열의 값을 문자열로 읽어서 imsi에 저장
-				imsi.setCode(rs.getNString("code"));
-				imsi.setName(rs.getNString("name"));
-				imsi.setManufacture(rs.getNString("manufacture"));
+				// MyBatis 나 Hibernate를 사용할 때 변수이름만 설정하면
+				// 이 작업은 할 필요가 없음
+				imsi.setCode(rs.getString("code"));
+				imsi.setName(rs.getString("name"));
+				imsi.setManufacture(rs.getString("manufacture"));
 				imsi.setPrice(rs.getInt("price"));
 				imsi.setReceivedate(rs.getDate("receivedate"));
 
@@ -79,58 +85,64 @@ public class GoodDAO {
 				list.add(imsi);
 			}
 		} catch (Exception e) {
-			System.out.println("SQL 실행 에러");
-			System.out.println(e.getMessage());
+			System.err.println("SQL 실행 에러");
+			System.err.println(e.getMessage());
 		}
+
 		// 데이터베이스 연결 해제
 		close();
+
 		return list;
 	}
 
-	// code를 가지고 데이터를 잦아오는 메소드 // code는 기본키 // select * from goods where code=?
+	// code를 가지고 데이터를 찾아오는 메소드
+	// code는 기본키
+	// select * from goods where code = ?
 	// 하나의 행만 리턴하는 경우에는 데이터가 없으면 null을 리턴
 	public Good getGood(String code) {
 		Good good = null;
-
 		// 데이터베이스 연결
 		connect();
 
 		// 데이터베이스 작업
 		try {
 			// SQL 실행 객체 생성
-			pstmt = con.prepareStatement("select * from goods where trim(code) =? ");
-			pstmt.setString(1, code);
+			pstmt = con.prepareStatement("select * from goods " + "where upper(trim(code))=?");
+			pstmt.setString(1, code.toUpperCase());
 			// SQL 실행
 			ResultSet rs = pstmt.executeQuery();
 			// 데이터가 2개 이상 나올 수 없으므로 if로 처리
 			if (rs.next()) {
 				good = new Good();
-				good.setCode(rs.getNString("code"));
-				good.setName(rs.getNString("name"));
-				good.setManufacture(rs.getNString("manufacture"));
+				good.setCode(rs.getString("code"));
+				good.setName(rs.getString("name"));
+				good.setManufacture(rs.getString("manufacture"));
 				good.setPrice(rs.getInt("price"));
 				good.setReceivedate(rs.getDate("receivedate"));
 			}
-		} catch (Exception e) {
-			System.out.println("상세보기 에러");
-			System.out.println(e.getMessage());
-		}
 
+		} catch (Exception e) {
+			System.err.println("상세보기 에러");
+			System.err.println(e.getMessage());
+		}
 		// 데이터베이스 연결 해제
 		close();
 		return good;
 	}
 
-	// 데이터를 삽입하는 메소드 // 삽입이나 수정은 매개변수가 DTO아니면 Map // -1이 리턴되면 실패 0이 리턴되면 조건에 맞는 데이터가
-	// 없음
+	// 데이터를 삽입하는 메소드
+	// 삽입이나 수정은 매개변수가 DTO 아니면 Map
+	// -1이 리턴되면 실패 0이 리턴되면 조건에 맞는 데이터가 없음
 	// 양수가 리턴되면 작업을 수행한 것임
-	public int intsrtGood(Good good) {
+	public int insertGood(Good good) {
 		int result = -1;
 		// 데이터베이스 연결
 		connect();
+
 		try {
-			pstmt = con
-					.prepareStatement("insert into goods (code,name,manufacture,price,receivedate) values(?,?,?,?,?) ");
+			// 데이터를 삽입하는 SQL 만들기
+			pstmt = con.prepareStatement(
+					"insert into goods(" + "code, name, manufacture, " + "price, receivedate) values(" + "?,?,?,?,?) ");
 			pstmt.setString(1, good.getCode());
 			pstmt.setString(2, good.getName());
 			pstmt.setString(3, good.getManufacture());
@@ -139,9 +151,6 @@ public class GoodDAO {
 
 			// 실행
 			result = pstmt.executeUpdate();
-
-			pstmt.close();
-			con.close();
 		} catch (Exception e) {
 			System.out.println("삽입 에러");
 			System.out.println(e.getMessage());
@@ -150,15 +159,84 @@ public class GoodDAO {
 		return result;
 	}
 
-	// 데이터를 수정하는 메소드 // 삽입하는 메소드와 동일한 모양
+	// 데이터를 수정하는 메소드
+	// 삽입하는 메소드와 동일한 모양
 	public int updateGood(Good good) {
 		int result = -1;
+		connect();
+
+		try {
+			pstmt = con.prepareStatement("update goods " + "set name=?, manufacture=?, price=? " + "where code = ?");
+
+			pstmt.setString(1, good.getName());
+			pstmt.setString(2, good.getManufacture());
+			pstmt.setInt(3, good.getPrice());
+			pstmt.setString(4, good.getCode());
+
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			System.out.println("데이터 수정 에러");
+			System.out.println(e.getMessage());
+		}
+
+		close();
 		return result;
 	}
 
-	// 데이터를 삭제하는 메소드// 삭제는 대부분의 경우 기본키만으로 수행
+	// 데이터를 삭제하는 메소드
+	// 삭제는 대부분의 경우 기본키만으로 수행
 	public int deleteGood(String code) {
 		int result = -1;
+		connect();
+		try {
+			pstmt = con.prepareStatement("delete from goods " + "where code = ?");
+			pstmt.setString(1, code);
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("데이터 삭제 에러");
+			System.out.println(e.getMessage());
+		}
+
+		close();
 		return result;
+	}
+
+	// 매개변수가 name 이나 manufacture에 포함된 데이터를
+	// 조회하는 메소드
+	// List, DTO 나 MAP, Scala(기본형, String, Date)
+	public List<Good> search(String word) {
+		// List는 생성해서 리턴
+		List<Good> list = new ArrayList<Good>();
+		connect();
+		try {
+			// 대소문자 구분을 하지 않는 검색은
+			// sql에서도 대문자나 소문자로 바꾸고
+			// 언어에서의 문자열도 대문자나 소문자로 변경
+			pstmt = con.prepareStatement(
+					"select * from goods where " + "upper(name) like ? or " + "upper(manufacture) like ?");
+			pstmt.setString(1, "%" + word.toUpperCase() + "%");
+			pstmt.setString(2, "%" + word.toUpperCase() + "%");
+
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Good good = new Good();
+
+				good.setCode(rs.getString("code"));
+				good.setName(rs.getString("name"));
+				good.setManufacture(rs.getString("manufacture"));
+				good.setPrice(rs.getInt("price"));
+				good.setReceivedate(rs.getDate("receivedate"));
+
+				list.add(good);
+			}
+
+			rs.close();
+		} catch (Exception e) {
+			System.out.println("데이터 검색 에러");
+			System.out.println(e.getMessage());
+		}
+		close();
+		return list;
 	}
 }
--- Java Application0513/src/javaapp0513/GoodMain.java
+++ Java Application0513/src/javaapp0513/GoodMain.java
@@ -4,14 +4,17 @@ import java.sql.Date;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class GoodMain {
-//static 초기화
+
+	// static 초기화
 	static {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			System.out.println("드라이버 클래스 로드 성공");
-		} catch (Exception e) {
-			// 드라이버 클래스 로드 성공 했는지 확인후 주석 처리 System.out.println(e.getMessage());
+			// System.out.println("드라이버 클래스 로드 성공");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
 	}
@@ -20,31 +23,34 @@ public class GoodMain {
 		// 키보드로 부터 입력받기 위한 인스턴스 생성
 		Scanner sc = new Scanner(System.in);
 
-		// GoodDAO 인스턴스를 생성
+		// GoodDAO 인스턴스 생성
 		GoodDAO dao = GoodDAO.sharedInstance();
+
 		mainloop: while (true) {
-			System.out.println("메뉴 입력(1.전체 데이터 보기 2.코드로 데이터 조회하기 3.데이터 삽입 4.데이터 수정 5.데이터 삭제 6.종료)");
+		System.out.print("메뉴 입력(1.전체 데이터 보기 2.코드로 데이터 조회하기 " + "3.데이터 삽입 4.데이터 수정 5.데이터 삭제 6.이름이나 원산지로 조회 7.종료):");
 			String menu = sc.nextLine();
+
 			// switch 구문 내에서 사용할 변수
 			List<Good> list = null;
 			Good good = null;
 			int result = -1;
 
 			String code = null;
+
 			switch (menu) {
 			case "1":
-				// 전체데이터 가져오는 메소드 호출
+				// 전체 데이터 가져오는 메소드 호출
 				list = dao.allGood();
 				// list 순회하면서 출력하기
 				for (Good g : list) {
-					System.out.println(g.getCode() + ":" + g.getName() + ":" + g.getPrice() + "원");
+					System.out.println(g.getCode() + "\t" + g.getName() + "\t" + g.getPrice() + "원");
 				}
-				System.out.println("전체 데이터 출력");
 				break;
 			case "2":
-				// 코드1개 입력받기
-				System.out.println("조회할 코드 입력");
+				// 코드 1개 입력받기
+				System.out.print("조회할 코드 입력:");
 				code = sc.nextLine();
+
 				// Dao 메소드 호출
 				good = dao.getGood(code);
 				if (good == null) {
@@ -52,10 +58,11 @@ public class GoodMain {
 				} else {
 					System.out.println(good);
 				}
+
 				break;
 			case "3":
-				// 삽입할 코드를입력
-				System.out.println("삽입할 코드를 입력");
+				// 삽입할 코드를 입력
+				System.out.print("삽입할 코드를 입력:");
 				code = sc.nextLine();
 				// 코드에 해당하는 데이터를 가져오기
 				good = dao.getGood(code);
@@ -65,44 +72,107 @@ public class GoodMain {
 					System.out.print("이름 입력:");
 					String name = sc.nextLine();
 					System.out.print("원산지 입력:");
-					String Manufacture = sc.nextLine();
+					String manufacture = sc.nextLine();
 					System.out.print("가격 입력:");
 					String temp = sc.nextLine();
 					int price = Integer.parseInt(temp);
 					// 현재 날짜 생성
 					Date receivedate = new Date(System.currentTimeMillis());
+
 					// 삽입할 데이터 생성
 					good = new Good();
 					good.setCode(code);
 					good.setName(name);
-					good.setManufacture(Manufacture);
+					good.setManufacture(manufacture);
 					good.setPrice(price);
 					good.setReceivedate(receivedate);
 
 					// sql 실행
-					result = dao.intsrtGood(good);
+					result = dao.insertGood(good);
+
 					if (result > 0) {
 						System.out.println("데이터 삽입 성공");
 					} else {
 						System.out.println("데이터 삽입 실패");
 					}
+
 				}
 				// 코드에 해당하는 데이터가 있으면
 				else {
-					System.out.println("이미 존재하는 코드");
+					System.out.println("이미 존재하는 코드입니다.");
 				}
+
 				break;
 			case "4":
-				System.out.println("데이터 수정");
+				while (true) {
+					System.out.print("수정할 코드를 입력:");
+					code = sc.nextLine();
+
+					good = dao.getGood(code);
+					// 데이터가 존재하는 경우
+					if (good != null) {
+						System.out.print("수정할 이름:");
+						String name = sc.nextLine();
+						good.setName(name);
+						System.out.print("수정할 원산지:");
+						String manufacture = sc.nextLine();
+						good.setManufacture(manufacture);
+						// 숫자 데이터는 되도록이면 문자열로 받은 후
+						// 정수로 변환하는 것을 권장
+						System.out.print("수정할 가격:");
+						String imsi = sc.nextLine();
+						good.setPrice(Integer.parseInt(imsi));
+
+						result = dao.updateGood(good);
+						if (result > 0) {
+							System.out.println("데이터 수정 성공");
+						} else if (result == 0) {
+							System.out.println("조건에 맞는 데이터 없음");
+						} else {
+							System.out.println("데이터 수정 실패");
+						}
+
+						break;
+					} else {
+						System.out.println("수정할 수 없는 코드입니다.");
+					}
+				}
 				break;
 			case "5":
-				System.out.println("데이터 삭제");
+				System.out.print("삭제할 코드 입력:");
+				code = sc.nextLine();
+				good = dao.getGood(code);
+				if (good == null) {
+					System.out.println("없는 코드입니다.");
+				} else {
+					// 대화상자를 출력해서 묻기
+					int r = JOptionPane.showConfirmDialog(null, "정말로 삭제", "삭제", JOptionPane.YES_NO_OPTION);
+					// System.out.println(r);
+					if (r == 0) {
+						// 데이터 삭제
+						result = dao.deleteGood(code);
+						if (result > 0) {
+							JOptionPane.showMessageDialog(null, "삭제 성공");
+						}
+					}
+				}
 				break;
 			case "6":
-				System.out.println("프로그램 종료");
-				// break mainloop라는 반복문을 종료
-				break mainloop;
+				System.out.print("이름이나 원산지를 입력:");
+				String word = sc.nextLine();
 
+				// 데이터베이스 처리 메소드 호출
+				List<Good> ar = dao.search(word);
+
+				for (Good g : ar) {
+					System.out.println(g);
+				}
+
+				break;
+			case "7":
+				System.out.println("프로그램 종료");
+				// mainloop 라는 반복문을 종료
+				break mainloop;
 			default:
 				System.out.println("잘못된 메뉴를 입력하셨습니다.");
 				break;
