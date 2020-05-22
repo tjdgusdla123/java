02cd48cd15db41769c1c27108b93dd805e80f33a
tjdgusdla123 <dkxkgh98@naver.com>
2020-05-22 12:29:28 +0900

    0522

---
+++ Java Application0522/src/javaapp0522/CheckMain.java
@@ -0,0 +1,11 @@
+package javaapp0522;
+
+public class CheckMain {
+
+	public static void main(String[] args) {
+		String STRING ="Hello CheckStyle";
+		System.out.println(STRING);
+
+	}
+
+}
---
+++ Java Application0522/src/javaapp0522/MySQLDriverload.java
@@ -0,0 +1,26 @@
+package javaapp0522;
+/**
+ * 
+ * Maven 연습을 위한 클래스
+ * 이렇게 만들면 클래스 주석입니다
+ *
+ */
+public class MySQLDriverload {
+/**
+ * 
+ * 실행을 위한 메소드 입니다
+ */
+	
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("드라이버 로드 성공");
+		}catch(Exception e) {
+			System.out.println("드라이버 로드 실패");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+}
