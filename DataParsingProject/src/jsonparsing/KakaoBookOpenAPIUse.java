b09c98ee0d8aa812ed960e2517ee4a085a7e2caa
tjdgusdla123 <dkxkgh98@naver.com>
2020-05-27 18:44:24 +0900

    0527

--- DataParsingProject/src/jsonparsing/KakaoBookOpenAPIUse.java
+++ DataParsingProject/src/jsonparsing/KakaoBookOpenAPIUse.java
@@ -17,154 +17,192 @@ import org.json.JSONObject;
 public class KakaoBookOpenAPIUse {
 
 	public static void main(String[] args) {
-		// 1.web에서 문자열 다운로드 받기
+		//1.Web에서 문자열 다운로드 받기
 		String jsonString = null;
 		String keyword = null;
 		try {
+			//다운로드 받을 URL을 생성
 			Scanner sc = new Scanner(System.in);
 			System.out.print("조회할 도서명:");
 			keyword = sc.nextLine();
-			// 영문과 숫자 이외의 데이터가 있을 수있으므로 인코딩
+			//영문과 숫자 이외의 데이터가 있을 수 있으므로 인코딩
 			keyword = URLEncoder.encode(keyword, "utf-8");
-
-			String addr = "https://dapi.kakao.com/v3/search/book?target=title&query=" + keyword;
+			
+			String addr = 
+				"https://dapi.kakao.com/v3/search/book?"
+				+ "target=title&size=50&query=" + keyword;
 			URL url = new URL(addr);
-
-			// URL에 연결 객체 생성
-
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			// 옵션설정
+			
+			//URL에 연결 객체 생성
+			HttpURLConnection con = 
+					(HttpURLConnection)url.openConnection();
 			con.setConnectTimeout(30000);
 			con.setUseCaches(false);
-
-			// 헤더 설정
-			con.addRequestProperty("Authorization", "KakaoAK 3c4960028e581ab2514e2be820cb4398");
-
-			sc.close();
-
-			// 문자열 읽어오기
+			//헤더 설정
+			con.addRequestProperty("Authorization",
+				"KakaoAK ae6b2875ee4452804ed4e01890078a7e");
+			
+			//문자열 읽어오기
 			StringBuilder sb = new StringBuilder();
-			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-			while (true) {
+			BufferedReader br = 
+				new BufferedReader(
+					new InputStreamReader(
+						con.getInputStream()));
+			while(true) {
 				String line = br.readLine();
-				if (line == null) {
+				if(line == null) {
 					break;
 				}
 				sb.append(line + "\n");
 			}
+			
 			jsonString = sb.toString();
+			
 			br.close();
 			con.disconnect();
-
 			sc.close();
-
-		} catch (Exception e) {
+		}catch(Exception e) {
 			System.out.println("다운로드 실패");
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		// System.out.println(jsonString);
-
-		// 읽어온 데이터에서 조회된 데이터 건수 찾아오기
-		// 조회된 데이터 개수를 저장할 변수
+		
+		//System.out.println(jsonString);
+		
+		//읽어온 데이터에서 조회된 데이터 건수 찾아오기
+		//조회된 데이터 개수를 저장할 변수
 		int total_count = -1;
 		try {
-			// 문자열을 JSONObject로 변경
-			JSONObject json = new JSONObject(jsonString);
-			// meta 속성의 내용을 JSONObject로 가져오기
-			JSONObject meta = json.getJSONObject("meta");
-			// System.out.println(meta);
-			// total_count 속성의 값을 정수로 가져오기
+			//문자열을 JSONObject로 변경
+			JSONObject json = 
+				new JSONObject(jsonString);
+			//meta 속성의 내용을 JSONObject로 가져오기
+			JSONObject meta = 
+					json.getJSONObject("meta");
+			//System.out.println(meta);
+			//total_count 속성의 값을 정수로 가져오기
 			total_count = meta.getInt("total_count");
-			// System.out.println(total_count);
-
-		} catch (Exception e) {
+			//System.out.println(total_count);
+			
+		}catch(Exception e) {
 			System.out.println("데이터 개수 가져오기 실패");
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		// 3.페이지 개수 구하기.
-		// 페이지 당 출력할 개수를 설정
+		
+		//3.페이지 개수 구하기
+		//페이지 당 출력할 개수를 설정
 		int perPage = 50;
-		// 페이지 개수를 계산 : 전체데이터 개수 (total_count)와 페이지당 출력개수 (perPahe)를 이용
-		// 서버 프로그래밍에서는 출력할 페이지 개수를 설정하는데 이용하고 클라이언트는 프로그래밍에서는 읽어야 할 페이지 개수를 설정하는데 이용
-		int pageCnt = (int) ((double) total_count / perPage + (double) (perPage - 1) / perPage);
-		// System.out.println(pageCnt);
+		//페이지 개수를 계산 
+		//전체 데이터 개수(total_count)와 
+		//페이지 당 출력 개수(perPage)를 이용
+		//서버 프로그래밍에서는 출력할 페이지 개수를 설정하는데
+		//이용하고 클라이언트 프로그래밍에서는 읽어야 할 페이지 개수를
+		//설정하는데 이용
+		int pageCnt = (int)
+				((double)total_count/perPage + 
+				(double)(perPage-1)/perPage);
+		//System.out.println(pageCnt);
 
-		// 데이터를 저장할 변수
-		List<Map<String, Object>> list = new ArrayList<>();
+		
+		//데이터를 저장할 변수
+		List<Map<String, Object>> list = 
+				new ArrayList<>();
 		try {
-			// 반복해서 데이터 가져와서 파싱
-			// 예외가 발생한 경우 다음 작업을 수행하도록 하는 경우에는 반복문 안에 예외 처리 구문을 만들어야 합니다.
-			// 웹의 데이터를 수집하는 경우 이런 방식으로 처리하지 않으면 처음 조회할 때는 있었는데
-			// 수집할 때 업어지면 예외가 발생해서 데이터를 수집하지 못하는 경우가 발생
-			for (int i = 1; i <= pageCnt; i++) {
+			//반복해서 데이터 가져와서 파싱
+			//예외가 발생한 경우 다음 작업을 수행하도록 하는 경우에는
+			//반복문 안에 예외 처리 구문을 만들어야 합니다.
+			//웹의 데이터를 수집하는 경우 이런 방식으로 처리하지 않으면
+			//처음 조회할 때는 있었는데 수집할 때 없어지면
+			//예외가 발생해서 데이터를 수집하지 못하는 경우가 발생
+			for(int i=1; i<=pageCnt; i=i+1) {
 				try {
-					// 다운로드 받을 url 만들기 -파라미터에 한글있는지 확인
-					String addr = "https://dapi.kakao.com/v3/search/book?target=title&query=" + keyword + "&page=" + i
-							+ "&size=" + perPage;
+					//다운로드 받을 URL 만들기 - 파라미터에 한글있는지 확인
+					String addr = 
+						"https://dapi.kakao.com/v3/search/book?"
+						+ "target=title&query=" + keyword 
+						+ "&page=" + i + "&size=" + perPage;
 					URL url = new URL(addr);
-					// 연결 객체 만들고 옵션 설정
-					// 문자열을 읽어서 출력해보고 한글이 깨지는지 확인
-					// 헤더 설정을 해야하는 확인
-					HttpURLConnection con = (HttpURLConnection) url.openConnection();
+					//연결 객체 만들고 옵션 설정
+					//문자열을 읽어서 출력해보고 한글이 깨지는지 확인
+					//헤더 설정을 해야 하는지 확인
+					HttpURLConnection con = 
+						(HttpURLConnection)url.openConnection();
 					con.setConnectTimeout(30000);
 					con.setUseCaches(false);
-					// 헤더 설정 - 옵션
-					con.addRequestProperty("Authorization", "KakaoAK 3c4960028e581ab2514e2be820cb4398");
-					// 문자열 가져오기
+					//헤더 설정 - 옵션
+					con.addRequestProperty("Authorization",
+				"KakaoAK ae6b2875ee4452804ed4e01890078a7e");
+					
+					//문자열 가져오기
 					String imsiString = null;
 					StringBuilder sb = new StringBuilder();
-					// 읽어온 데이터의 한글이 깨지 때 utf-8을 euc-kr로 변경
-					BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
-					while (true) {
+					//읽어온 데이터의 한글이 깨질 때 utf-8을 euc-kr로 변경
+					BufferedReader br = 
+						new BufferedReader(
+							new InputStreamReader(
+								con.getInputStream(), "utf-8"));
+					while(true) {
 						String line = br.readLine();
-						if (line == null) {
+						if(line == null) {
 							break;
 						}
 						sb.append(line + "\n");
 					}
 					imsiString = sb.toString();
-					// System.out.println(imsiString);
+					//System.out.println(imsiString);
 					br.close();
 					con.disconnect();
-
-					// 읽어온 문자열을 JSONObject로 변환
+					
+					//읽어온 문자열을 JSONObject로 변환
 					JSONObject obj = new JSONObject(imsiString);
-					// documents속성의 내용을 배열로 가져오기
-					JSONArray documents = obj.getJSONArray("documents");
-					// 배열순회
-					for (int j = 0; j < documents.length(); j++) {
-						// 배열을 순회 하면서 데이터 가져오기
-						JSONObject document = documents.getJSONObject(i);
-						// 객체에서 데이터 가져오기
-						String title = document.getString("title");
-						String urlStr = document.getString("url");
-						int price = document.getInt("price");
-						//map으로 위의 데이터 묶기
-						Map<String,Object> map= new HashMap<>();
-						map.put("title",title );
-						map.put("url",url );
-						map.put("price",price );
-						//list에 추가합니다.
+					//documents 속성의 내용을 배열로 가져오기
+					JSONArray documents = 
+							obj.getJSONArray("documents");
+					//배열 순회
+					for(int j=0; j<documents.length(); j=j+1 ) {
+						//배열을 순회하면서 데이터 가져오기
+						JSONObject document = 
+							documents.getJSONObject(j);
+						//객체에서 데이터 읽어오기
+						String title = 
+							document.getString("title");
+						String urlStr = 
+							document.getString("url");
+						int price = 
+							document.getInt("price");
+						//Map으로 위의 데이터 묶기
+						Map<String, Object> map = 
+							new HashMap<>();
+						map.put("title", title);
+						map.put("url", urlStr);
+						map.put("price", price);
+						//list에 추가
 						list.add(map);
 					}
-				} catch (Exception e) {
-					System.out.println("실패 하면 다음 페이지도 이동");
+				}catch(Exception e) {
+					System.out.println(
+							"실패 하면 다음 페이지도 이동");
 					System.out.println(e.getMessage());
 					e.printStackTrace();
 				}
+				
 			}
-		} catch (Exception e) {
+		}catch(Exception e) {
 			System.out.println("페이지 별로 가져오기 실패");
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-
-		for(Map<String,Object>map : list) {
+		//가져온 데이터 출력
+		for(Map<String, Object> map : list) {
 			System.out.println(map.get("title"));
 		}
 	}
-
 }
+
+
+
+
+
+
+
--- DataParsingProject/src/jsonparsing/MovieParsing.java
+++ DataParsingProject/src/jsonparsing/MovieParsing.java
@@ -6,7 +6,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,142 +14,176 @@ import java.util.Map;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.sql.PreparedStatement;
+
 public class MovieParsing {
 
 	public static void main(String[] args) {
-		// 다운로드 받을 문자열을 저장할 변수
+	
+		//다운로드 받을 문자열을 저장할 변수
 		String jsonString = null;
-
-		// 웹에서 다운 받기
+		//웹에서 다운로드 받기
 		try {
-			// 다운로드 받을 주소 만들기
+			//다운로드 받을 주소 만들기
 			String addr = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=1&count=20&genreId=&order=releasedatease";
-			// URL에 한글이 있으면 한글 부분은 utf-8로 인코딩해야합니다.
-			// URLEncoder.encode("한글문;자열","utf-8")
+			//URL에 한글이 있으면 한글 부분은 utf-8로 인코딩해야 합니다.
+			//URLEncoder.encode("한글문자열", "utf-8")
 			URL url = new URL(addr);
-
-			// HttpURLCoonection생성
-			// openConnection은 HttpURLConnection이라는 추상 클래스 타입으로 리턴하므로 강제 형 변환 해서 일반클래스 타입으로
-			// 변경
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			// 옵션설정
-			con.setConnectTimeout(3000);// 최대 연결 시간 설정
-			con.setUseCaches(false);// 이전에 받은 데이터 사용 여부
-
-			// 문자열을 읽을 스트림을 생성
-			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-
-			// 문자열을 읽기 위한 임시변수를 생성
-			StringBuilder sb = new StringBuilder();
-			while (true) {
-				// 한줄 읽어오기
+			
+			//HttpURLConnection 생성
+			//openConnection은 URLConnection이라는 
+			//추상 클래스 타입으로 리턴하므로 강제 형 변환을 해서
+			//일반 클래스 타입으로 변경
+			HttpURLConnection con = 
+				(HttpURLConnection) url.openConnection();
+			//옵션 설정
+			con.setConnectTimeout(30000);//최대 연결 시간 설정
+			con.setUseCaches(false);//이전에 받은 데이터 사용 여부
+			
+			//문자열을 읽을 스트림을 생성
+			//읽은 내용이 깨질 때는 con.getInputStream
+			//다음에 , "인코딩 방식" 을 추가
+			BufferedReader br = 
+				new BufferedReader(
+					new InputStreamReader(
+						con.getInputStream()));
+			//문자열을 읽기 위한 임시변수를 생성
+			StringBuilder sb = 
+					new StringBuilder();
+			while(true) {
+				//한 줄 읽어오기
 				String line = br.readLine();
-				// 읽은 데이터가 없으면 종료
-				if (line == null) {
+				//읽은 데이터가 없으면 종료
+				if(line == null) {
 					break;
 				}
-				// 읽은 데이터가ㅣ 있으면 sb에추가
+				//읽은 데이터가 있으면 sb에 추가
 				sb.append(line + "\n");
 			}
-
-			// 연결종료
+			
+			//연결 종료
 			br.close();
 			con.disconnect();
-			// 읽은 내용을 String으로 변환
+			//읽은 내용을 String으로 변환
 			jsonString = sb.toString();
-
-		} catch (Exception e) {
+			
+		}catch(Exception e) {
 			System.out.println("다운로드 실패");
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		// System.out.println(jsonString);
-
-		// 파싱 결과를 저장할 변수를 생성
-		// title 과 ratingAverage를 저장
-		// title 과 ratingAverage를map에 저장
-		List<Map<String, Object>> list = new ArrayList<>();
-
-		// 데이터 파싱
+		//System.out.println(jsonString);
+		
+		//파싱 결과를 저장할 변수를 생성
+		
+		//title 과 ratingAverage를 저장
+		//title 과 ratingAverage를 Map에 저장
+		List<Map<String, Object>> list = 
+				new ArrayList<>();
+				
+		//데이터 파싱
 		try {
-			// 텍스트가 존재하는 경우만 수행
-			// &&를 만들때는 null 체크를 먼저
-			if (jsonString != null && jsonString.trim().length() > 0) {
-				// 첫번째는 JSON 개체
-				JSONObject mainData = new JSONObject(jsonString);
-				// System.out.println(mainData);
-
-				// hoppin이라는 key의 값을 객체로 가져오기
-				JSONObject hoppin = mainData.getJSONObject("hoppin");
-				// System.out.println(hoppin);
-				JSONObject movies = hoppin.getJSONObject("movies");
-				// System.out.println(movies);
-				JSONArray movie = movies.getJSONArray("movie");
-				// System.out.println(movie);
-				for (int i = 0; i < movie.length(); i = i + 1) {
-					JSONObject imsi = movie.getJSONObject(i);
-					// System.out.println(imsi);
-
-					// 데이터를 추출
+			//텍스트가 존재하는 경우만 수행
+			//&&를 만들 때는 null 체크를 먼저
+			if( jsonString != null &&  
+				jsonString.trim().length() > 0
+				) {
+				//첫번째는 JSON 객체
+				JSONObject mainData = 
+					new JSONObject(jsonString);
+				//System.out.println(mainData);
+				
+				//hoppin이라는 key의 값을 객체로 가져옥
+				JSONObject hoppin = 
+						mainData.getJSONObject("hoppin");
+				//System.out.println(hoppin);
+				
+				JSONObject movies = 
+						hoppin.getJSONObject("movies");
+				//System.out.println(movies);
+				
+				JSONArray movie = 
+						movies.getJSONArray("movie");
+				//System.out.println(movie);
+				
+				for(int i=0; i<movie.length(); i=i+1) {
+					JSONObject imsi = 
+							movie.getJSONObject(i);
+					//System.out.println(imsi);
+					
+					//데이터를 추출
 					String title = imsi.getString("title");
-					String ratingAverage = imsi.getString("ratingAverage");
-					// Map으로 생성
-					Map<String, Object> map = new HashMap<>();
+					String ratingAverage = 
+							imsi.getString("ratingAverage");
+					//Map으로 생성
+					Map<String, Object> map = 
+						new HashMap<>();
 					map.put("title", title);
-					map.put("ratingAverage", Double.parseDouble(ratingAverage));
-					// list에 추가
+					map.put("ratingAverage", 
+						Double.parseDouble(ratingAverage));
+					//list에 추가
 					list.add(map);
 				}
-
-			} else {
+				
+			}else {
 				System.out.println("다운로드 받은 문자열이 없음");
-				// 프로그램 종료
+				//프로그램 종료
 				System.exit(0);
 			}
-		} catch (Exception e) {
+		}catch(Exception e) {
 			System.out.println("파싱 실패");
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		// System.out.println(list);
-
-		// 데이터베이스에 저장
+		
+		//System.out.println(list);
+		
+		//데이터베이스에 저장
 		try {
-			// 드라이버 클래스 로드
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			// 이 메시지가 출력안되면
-			// mysql Driver를 프로젝트에 삽입하지않았거나 드리이버 클래스 이름이 잘못됨
-
-			// 데이터베이스 연결
-			// mysql은 한글이 있는 경우 연결 문자열에 유니코드 사용을 명시해야 합니다.
-			// ?useUnicode=true&characterEncoding=utf8
-
-			// MySQL과 java의driver와 버전이 맞이 않아서
-			// timeZone에러가 발생하느 경우 추가
-			// &serverTimezone=UTC
-
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sample"
-					+ "?useUnicode=true&characterEncoding=utf8" + "&serverTimezone=UTC", "root", "8989450a");
-			// System.out.println(con);
-
-			for(Map<String,Object>map: list) {
-				PreparedStatement pstmt = con.prepareStatement(" insert into movie(title , ratingaverage) "+" values(?,?) ");
-				pstmt.setString(1,(String)map.get("title"));                            
-				pstmt.setDouble(2,(Double)map.get("ratingAverage"));
-				                                  
+			//드라이버 클래스 로드
+			Class.forName("com.mysql.jdbc.Driver");
+			//이 메시지가 출력안되면
+			//MySQL Driver를 프로젝트에 삽입하지 않았거나
+			//드라이버 클래스 이름이 잘못됨
+			//System.out.println("드라이버 클래스 로드");
 			
-			//SQL실행 
-			pstmt.executeUpdate();
+			//데이터베이스 연결
+			//MySQL은 한글이 있는 경우 연결 문자열에 
+			//유니코드 사용을 명시해야 합니다.
+			//?useUnicode=true&characterEncoding=utf8
 			
-			pstmt.close();
+			//MySQL 과 java의 driver와 버전이 맞지 않아서
+			//timeZone에러가 발생하는 경우 추가
+			//&serverTimezone=UTC
+			Connection con = 
+				DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/sample"
+				+ "?useUnicode=true&characterEncoding=utf8"
+				+ "&serverTimezone=UTC",
+				"root","*******");
+			//System.out.println(con);
 			
+			//list를 순회하면서 데이터를 테이블에 삽입하기
+			for(Map<String, Object> map : list) {
+				//SQL 실행 객체 만들기
+				PreparedStatement pstmt = 
+					con.prepareStatement(
+						"insert into movie(title, ratingaverage) "
+						+ "values(?,?)");
+				pstmt.setString(1, 
+					(String)map.get("title"));
+				pstmt.setDouble(2, 
+					(Double)map.get("ratingAverage"));
+				
+				//SQL 실행
+				pstmt.executeUpdate();
+				
+				pstmt.close();
 			}
 			
-			
-			// 연결객체 닫기
+			//연결객체 닫기
 			con.close();
-			System.out.println("드라이버 클래스 로드");
-		} catch (Exception e) {
+		}catch(Exception e) {
 			System.out.println("데이터베이스 작업 실패");
 			System.out.println(e.getMessage());
 			e.printStackTrace();
